4e348f70ab8cd0b7760ef3fcb6abf3bbc5d4dd2c
idysfunctionalCoding <Parker.Birchard@gmail.com>
2024-07-22 22:09:29 -0400

    trying to fix app router

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,11 @@ import customTheme from "./theme";
 export const App = () => (
   <ChakraProvider theme={customTheme}>
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/articles" element={<ArticlesPage />} />
-      <Route path="/worldmap" element={<WorldMap />} />
-      <Route path='/nations/holyearisampire' element={<IndividualNationPage {...earisEmpirePageCofig}/> } />
-      <Route path='/nations' element={<NationsInfoPage /> } />
+      <Route path="/dndworldwebsite" element={<HomePage />} />
+      <Route path="/dndworldwebsite/articles" element={<ArticlesPage />} />
+      <Route path="/dndworldwebsite/worldmap" element={<WorldMap />} />
+      <Route path='/dndworldwebsite/nations/holyearisampire' element={<IndividualNationPage {...earisEmpirePageCofig}/> } />
+      <Route path='/dndworldwebsite/nations' element={<NationsInfoPage /> } />
     </Routes>
   </ChakraProvider>
 );
